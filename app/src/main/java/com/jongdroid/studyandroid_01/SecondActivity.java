76db04bffd661d938fc3e89a50f466161612d3bc
hoonjong <qkr7627@naver.com>
2021-05-10 21:06:14 +0900

    2021-05-07 Start Github
    2021-05-10
     1.STUDY Toast message , Button event

--- app/src/main/java/com/jongdroid/studyandroid_01/SecondActivity.java
+++ app/src/main/java/com/jongdroid/studyandroid_01/SecondActivity.java
@@ -12,31 +12,28 @@ import androidx.appcompat.app.AppCompatActivity;
  *  안드로이드 학습 용도로 사용할 예정 2021.05.10
  *
  *  1.버튼 클릭 리스너와 버튼 onClick 속성을 이용한 토스트메시지
- *
+ *  
  */
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_second);
+        
         Button btn = findViewById(R.id.btn1);
-
+        
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "안녕하세요!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(SecondActivity.this, "", Toast.LENGTH_SHORT).show();
             }
         });
 
     }
-
-    public void onbutton(View v) {
-        Toast.makeText(MainActivity.this, "저도 안녕하세요!!", Toast.LENGTH_SHORT).show();
+    
+    public void onbutton (View v) {
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
     }
-
-
-
 }
 
